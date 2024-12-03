15ccb4ea0e03b1e2eb6b5f256196eca51d07028d
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2024-12-03 21:57:49 +0000

    Added tests for day3

---
+++ tests/test_day3.py
@@ -0,0 +1,9 @@
+from aoc24.day3 import part1, part2
+
+
+def test_part1():
+    assert part1(data_path="data/tests/day3a.txt") == 161
+
+
+def test_part2():
+    assert part2(data_path="data/tests/day3b.txt") == 48
